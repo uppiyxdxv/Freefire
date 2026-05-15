7f997e5b3485feb39b98bc42b1b3d286d02dc76e
uppiyxdxv <upendrapallolla@gmail.com>
2026-05-16 02:26:06 +0530

    fix databases

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
-// Alert Button
+// Firebase Configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-function showMessage(){
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDXFvX60p4H2-84fd0xM7WX_gUCIBQ0NIo",
+    authDomain: "freefirewebsite-59234.firebaseapp.com",
+    databaseURL: "https://freefirewebsite-59234-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "freefirewebsite-59234",
+    storageBucket: "freefirewebsite-59234.firebasestorage.app",
+    messagingSenderId: "683223451817",
+    appId: "1:683223451817:web:86bbbbeb0213a8d76201b8",
+    measurementId: "G-PFLY3DHEXX"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.database();
+
+// Database Reference
+
+const database = firebase.database();
+
+// Alert Message
+
+function showMessage() {
     alert("Welcome to Free Fire!");
 }
 
-// Form Validation
+// Contact Form
 
 document.getElementById("contactForm")
 .addEventListener("submit", function(event){
@@ -17,31 +47,56 @@ document.getElementById("contactForm")
     let email =
     document.getElementById("email").value;
 
+    let message =
+    document.getElementById("message").value;
+
     let error =
     document.getElementById("error");
 
     let emailPattern =
     /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-    if(name === "" || email === ""){
+    // Validation
+
+    if(name === "" || email === "" || message === ""){
+
+        error.style.color = "red";
+
         error.innerHTML =
         "All fields are required!";
     }
 
     else if(!email.match(emailPattern)){
+
+        error.style.color = "red";
+
         error.innerHTML =
         "Enter valid email!";
     }
 
     else{
+
+        // Store Data in Firebase
+
+        database.ref("contacts").push({
+            name: name,
+            email: email,
+            message: message
+        });
+
         error.style.color = "lightgreen";
+
         error.innerHTML =
         "Form Submitted Successfully!";
+
+        // Clear Form
+
+        document.getElementById("contactForm").reset();
     }
 
 });
 
-// DOM Manipulation To-Do List
+// To-Do List
 
 function addTask(){
 
@@ -71,3 +126,5 @@ function addTask(){
 function removeTask(button){
     button.parentElement.remove();
 }
+
+
